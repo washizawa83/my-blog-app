78b61d0f35f4d29711f1b86eed330656861a62fd
washizawa83 <washizawa83@gmail.com>
2025-04-20 18:14:35 +0900

    その他要素の修正

--- src/app/components/mdx/MdxLayout.tsx
+++ src/app/components/mdx/MdxLayout.tsx
@@ -6,21 +6,21 @@ type Props = {
 const textColor = 'text-slate-300'
 const utilConfig = `${textColor} max-w-full break-words`
 
-const h1Config = `prose-h1:text-5xl prose-h1:text-center`
-const h2Config = `prose-h2:text-3xl`
-const h3Config = `prose-h3:text-2xl`
-const h4Config = `prose-h4:text-xl`
-const h5Config = `prose-h5:text-lg`
-const h6Config = `prose-h6:text-l`
+const h1Config = `prose-h1:text-4xl prose-h1:py-12 prose-h1:pl-5 prose-h1:border-l-4 prose-h1:border-indigo-500`
+const h2Config = `prose-h2:text-2xl prose-h2:my-12`
+const h3Config = `prose-h3:text-xl prose-h2:my-12`
+const h4Config = `prose-h4:text-lg prose-h4:font-semibold`
+const h5Config = `prose-h5:text-md prose-h5:font-semibold`
+const h6Config = `prose-h6:text-sm prose-h6:font-semibold`
 
-const headingConfig = `prose-headings:text-slate-300 prose-headings:font-normal prose-headings:my-12 prose-headings:pb-2 ${h1Config} ${h2Config} ${h3Config} ${h4Config} ${h5Config} ${h6Config}`
+const headingConfig = `prose-headings:text-slate-300 prose-headings:font-normal prose-headings:my-8 prose-headings:pb-2 ${h1Config} ${h2Config} ${h3Config} ${h4Config} ${h5Config} ${h6Config}`
 
 const paragraphConfig =
-  'prose-p:text-base prose-p:whitespace-break-spaces prose-p:my-2'
+  'prose-p:text-base prose-p:whitespace-break-spaces prose-p:my-4 prose-p:leading-7'
 const strongConfig = 'prose-strong:text-slate-300 prose-strong:font-bold'
-const anchorLinkConfig = `prose-a:decoration-transparent prose-a:bg-orange-400/[.4] prose-a:rounded-xl prose-a:px-2 prose-a:hover:bg-orange-300/[.4] prose-a:text-slate-300`
+const anchorLinkConfig = `prose-a:decoration-transparent prose-a:bg-pink-400/[.7] prose-a:rounded-xl prose-a:px-2 prose-a:mx-1 prose-a:hover:bg-pink-500/[.8] prose-a:text-slate-300`
 
-const ulConfig = 'prose-ul:my-2'
+const ulConfig = 'prose-ul:my-2 prose-ul:marker:text-teal-500'
 
 const blockquoteConfig =
   'prose-blockquote:bg-neutral-500 prose-blockquote:rounded prose-blockquote:py-1 prose-blockquote:my-2'
