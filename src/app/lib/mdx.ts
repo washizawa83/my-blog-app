de3760cf16a83a5fd0c8987ffcc5e7285926cb21
washizawa83 <washizawa83@gmail.com>
2025-04-20 22:18:37 +0900

    シンタックスハイライト設定

--- src/app/lib/mdx.ts
+++ src/app/lib/mdx.ts
@@ -1,11 +1,26 @@
+import { CompileOptions } from "@mdx-js/mdx";
 import { serialize } from "next-mdx-remote/serialize"
 import rehypePrettyCode from 'rehype-pretty-code'
+
+export const MdxOptions: Omit<CompileOptions, 'outputFormat' | 'providerImportSource'> & {
+  useDynamicImport?: boolean;
+} = {
+  rehypePlugins: [
+    [
+      rehypePrettyCode,
+      {
+        theme: 'material-theme-ocean',
+        keepBackground: false,
+        defaultLang: 'ts',
+        showLineNumbers: true,
+      },
+    ],
+  ],
+}
 
 export const parseMdxString = async (content: string, data: {[key: string]: unknown}) => {
   return await serialize(content, {
     scope: data,
-    mdxOptions: {
-      rehypePlugins: [[rehypePrettyCode]],
-    },
+    mdxOptions: MdxOptions
   })
 }
