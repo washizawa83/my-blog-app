cda8ec50f05b7c7ac1fff9d41d7b2a73411b3cb3
washizawa83 <washizawa83@gmail.com>
2025-04-20 12:10:27 +0900

    ログ調査

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,10 @@ import { PageLayout } from './components/layouts/PageLayout'
 import { getArticleInfos } from './service/article/article'
 
 export default async function Home() {
+  console.log('記事データ取得')
   const articles = await getArticleInfos()
+  console.log(articles)
+  console.log('記事データ取得完了')
 
   return (
     <PageLayout>
