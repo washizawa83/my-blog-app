49450a21b5cf5b32b6f00f4ca744ec445b523c06
washizawa haruya (JSL) <129074090+washizawa83@users.noreply.github.com>
2025-04-21 20:48:54 +0900

    記事をサーバー側でレンダリング (#22)

--- src/app/lib/mdx.ts
+++ src/app/lib/mdx.ts
@@ -1,6 +1,8 @@
 import { CompileOptions } from "@mdx-js/mdx";
+import { compileMDX } from "next-mdx-remote/rsc";
 import { serialize } from "next-mdx-remote/serialize"
 import rehypePrettyCode from 'rehype-pretty-code'
+import { Button } from "../components/mdx/components/Button";
 
 export const MdxOptions: Omit<CompileOptions, 'outputFormat' | 'providerImportSource'> & {
   useDynamicImport?: boolean;
@@ -22,5 +24,17 @@ export const parseMdxString = async (content: string, data: {[key: string]: unkn
   return await serialize(content, {
     scope: data,
     mdxOptions: MdxOptions
+  })
+}
+
+export const parseMdxStringByRemote = async (data: string) => {
+  return compileMDX({
+    source: data,
+    options: {
+      mdxOptions: {
+        rehypePlugins: [[rehypePrettyCode, MdxOptions]],
+      },
+    },
+    components: {Button}
   })
 }
