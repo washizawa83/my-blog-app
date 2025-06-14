150abc31766d71c24ec9b35829e6a9c153fab11e
washizawa83 <washizawa83@gmail.com>
2025-06-14 15:53:48 +0900

    add note component

--- src/app/components/mdx/MdxLayout.tsx
+++ src/app/components/mdx/MdxLayout.tsx
@@ -9,10 +9,10 @@ type Props = {
 const textColor = 'text-slate-300'
 const utilConfig = `${textColor} break-words`
 
-const h1Config = `prose-h1:text-4xl prose-h1:mt-18 prose-h1:mb-0`
-const h2Config = `prose-h2:text-lg prose-h2:mt-16 prose-h2:mb-0`
-const h3Config = `prose-h3:text-base prose-h3:mt-12 prose-h3:mb-0`
-const h4Config = `prose-h4:text-sm prose-h4:mt-10 prose-h4:mb-0`
+const h1Config = `prose-h1:text-2xl prose-h1:mt-18 prose-h1:mb-0`
+const h2Config = `prose-h2:text-xl prose-h2:mt-16 prose-h2:mb-0`
+const h3Config = `prose-h3:text-lg prose-h3:mt-12 prose-h3:mb-0`
+const h4Config = `prose-h4:text-md prose-h4:mt-10 prose-h4:mb-0`
 const h5Config = ``
 const h6Config = ``
 
@@ -23,7 +23,7 @@ const paragraphConfig =
 const strongConfig = 'prose-strong:text-slate-200 prose-strong:font-bold'
 const anchorLinkConfig = `prose-a:text-slate-200 prose-a:font-semibold prose-a:mx-1 prose-a:decoration-transparent prose-a:border-b prose-a:hover:border-b-2 prose-a:border-emerald-600 prose-a:hover:border-emerald-600`
 
-const ulConfig = 'prose-ul:mt-6 prose-ul:mb-0 prose-ul:marker:text-slate-600'
+const ulConfig = 'prose-ul:mt-4 prose-ul:mb-0 prose-ul:marker:text-slate-600'
 
 const blockquoteConfig =
   'prose-blockquote:bg-neutral-500 prose-blockquote:rounded prose-blockquote:py-1 prose-blockquote:my-2'
